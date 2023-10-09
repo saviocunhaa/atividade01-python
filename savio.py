598645e843b7d3d84a40a7724d8cdd099e18316d
saviocunhaa <saviocunha.001@gmail.com>
2023-10-09 13:55:27 -0300

    atividade 01 ok

---
+++ savio.py
@@ -0,0 +1,9 @@
+# media de 3 numeros
+# receber as 3 notas
+nota1 = 8.0
+nota2 = 7.5
+nota3 = 4.7
+# Somar as notas e dividir por 3
+media = (nota1 + nota2 + nota3) / 3
+# apresentar a media
+print("sua  média é: 💕 ", media)
